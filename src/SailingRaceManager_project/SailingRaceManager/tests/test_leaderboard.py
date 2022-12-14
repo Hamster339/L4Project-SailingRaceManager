accdcf66f1f0d4017f263554465262ed522ed3db
Hamish Philip <hphilip92@gmail.com>
2022-12-14 17:07:51 +0000

    Blank testing suite and some documentaion

---
+++ src/SailingRaceManager_project/SailingRaceManager/tests/test_leaderboard.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.test import Client
+
+
+class Test_leaderboard(TestCase):
+
+    def setUpTest_leaderboard(self):
+        pass
+
+    def test_current_series(self):
+        pass
